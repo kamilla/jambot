6d6dea8e62a2cd8eb6a103e0d83e0c4d90f21d32
kamilla <joonas.greis@kamillaproductions.com>
2013-10-24 18:32:41 +0300

    added gtranslate module

--- build/lib/willie/modules/riinaaja.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from willie.module import commands
-
-@commands('.*[Hh][Uu][Oo][Mm][Ee][Nn][Tt][Aa]\s*.*[Jj][Aa][Mm][Bb][Oo][Tt].*|.*[Jj][Aa][Mm][Bb][Oo][Tt]\s*.*[Hh][Uu][Oo][Mm][Ee][Nn][Tt][Aa].*')
-def gmorning(bot, trigger):
-	if trigger.nick=='Riinaaja' or trigger.nick=='kamilla':
-		bot.say('Huomenta '+ trigger.nick +', sinä ihana otus <3')
